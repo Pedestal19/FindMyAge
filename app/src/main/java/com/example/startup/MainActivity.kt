60a5f63c6396f4e8f2f7dcf22b68fc39290f7fbf
Pedestal19 <gabeoji01@gmail.com>
2020-03-18 23:04:19 +0100

    refactored onclick to xml

--- app/src/main/java/com/example/startup/MainActivity.kt
+++ app/src/main/java/com/example/startup/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.startup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -11,13 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buFindAge.setOnClickListener {
-            val yearOfBirth:Int = txtYearOfBirth.text.toString().toInt()
-            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-            val myAge = currentYear - yearOfBirth
-            tvShowAge.text = "Your age is $myAge years"
+        /*buFindAge.setOnClickListener {
 
-        }
 
+        }*/
+
+    }
+
+    fun buFindAgeEvent(view: View){
+        val yearOfBirth:Int = txtYearOfBirth.text.toString().toInt()
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val myAge = currentYear - yearOfBirth
+        tvShowAge.text = "Your age is $myAge years"
     }
 }
